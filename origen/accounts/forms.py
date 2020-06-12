3889f2d3150b7cd3c289edfb7ae02e406ed7dfb4
Ryan Lider <rslider@shaw.ca>
2020-06-12 11:36:43 -0700

    Login/Signup Functionality

--- origen/accounts/forms.py
+++ origen/accounts/forms.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
+from accounts.models import Person
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Username', 'autocomplete': 'off'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'Password'}))
 
 class PersonCreateForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'First Name', 'autocomplete': 'off'}),label='')
--- origen/accounts/views.py
+++ origen/accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 from . import forms
 
 # Create your views here.
@@ -9,8 +11,13 @@ class SignUp(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
 
-class Login(CreateView):
-    form_class = forms.PersonCreateForm
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user, backend='django.contrib.auth.backends.ModelBackend')
+        return redirect('home')
+
+class Login(LoginView):
+    form_class = forms.LoginForm
     template_name = 'accounts/login.html'
 
 
