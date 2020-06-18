783f2439060f15b48def8cfcf7211e5aa17f0da6
jason <jasoncfleebcit@gmail.com>
2020-06-18 05:10:32 -0700

    added profile features, biography, job position, and alert

--- origen/accounts/models.py
+++ origen/accounts/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.contrib import auth
 # Create your models here.
 
-
 class Person(models.Model):
     user = models.OneToOneField(auth.models.User, on_delete=models.CASCADE, primary_key=True)
     # organization = models.ForeignKey()
     
+    biography = models.TextField(blank=True)
+    position = models.CharField(max_length=120, blank=True)
+    alert = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user
