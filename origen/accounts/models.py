116079643deb33e67aaf17aa9196614eaa73a9bd
wilsonzhu <wz.wilsonzhu@gmail.com>
2020-06-18 18:03:03 -0700

    Added time zones to settings page

--- origen/accounts/models.py
+++ origen/accounts/models.py
@@ -3,12 +3,19 @@ from django.contrib import auth
 # Create your models here.
 
 class Person(models.Model):
+
+    import pytz
+    pytz.all_timezones
+
+    TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+
     user = models.OneToOneField(auth.models.User, on_delete=models.CASCADE, primary_key=True)
     # organization = models.ForeignKey()
     
-    biography = models.TextField(blank=True)
-    position = models.CharField(max_length=120, blank=True)
-    alert = models.BooleanField(default=True)
+    biography = models.TextField(null=True, blank=True)
+    job_title = models.CharField(max_length=120, null= True, blank=True)
+    alerts = models.BooleanField(default=True)
+    time_zone = models.CharField(max_length=100, choices = TIMEZONES, default='PST')
 
     def __str__(self):
         return self.user
