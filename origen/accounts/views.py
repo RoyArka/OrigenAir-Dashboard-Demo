95a0f8540cb8a9d37accc5c683169a598c5e6297
Ryan Lider <rslider@shaw.ca>
2020-06-24 12:43:56 -0700

    Organization app Other_user context variable Misc profile changes

--- origen/accounts/views.py
+++ origen/accounts/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from django.http import Http404
 from . forms import PersonUpdateForm
 from . models import Person
 from . import forms
+User = get_user_model()
+
 
 # Create your views here.
 class SignUp(CreateView):
@@ -28,12 +31,23 @@ class UserProfile(ListView):
     model = Person
     template_name = 'accounts/profile.html' 
 
+    def get_queryset(self):
+        try:
+            self.other_user = User.objects.get(username__iexact=self.kwargs.get('username'))
+        except User.DoesNotExist:
+            raise Http404
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['other_user'] = self.other_user
+        return context
+
 class UserProfileUpdate(UpdateView):
     model = Person 
     form_class = PersonUpdateForm
     template_name = 'accounts/profile_update.html'
     def get_success_url(self):
-        return reverse_lazy('accounts:profile', user=user.username)
+        return reverse_lazy('accounts:profile', kwargs={'username': self.object.username})
 
     def get_object(self):
         return self.request.user
