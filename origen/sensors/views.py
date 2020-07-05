27312b59b156f7640501dde95211c61c9b0f1201
RoyArka <blueroy9@live.com>
2020-07-04 21:05:56 -0700

    basic layout for url and views for sensors app

--- origen/sensors/views.py
+++ origen/sensors/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import CreateView 
+from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView 
+from django.views.generic.detail import DetailView 
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+class CreateSensor(CreateView):
+    template_name = 'sensors/'
+
+class SensorDetail(DetailView):
+    template_name = 'sensors/' 
+
+class UpdateSensor(UpdateView):
+    template_name = 'sensors/'
