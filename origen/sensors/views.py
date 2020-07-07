1a648950715754ad80c19f86af8805273c29cbfd
jason <jasoncfleebcit@gmail.com>
2020-07-07 16:37:24 -0700

    added non-css create sensor page

--- origen/sensors/views.py
+++ origen/sensors/views.py
@@ -3,12 +3,15 @@ from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView 
 from django.views.generic.detail import DetailView 
 from django.shortcuts import render, redirect
+from . models import Sensors
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-class CreateSensor(CreateView):
+class CreateSensor(LoginRequiredMixin, ListView):
     template_name = 'sensors/sensors_create.html'
-
+    model = Sensors
+    
 class SensorDetail(DetailView):
     template_name = 'sensors/' 
 
