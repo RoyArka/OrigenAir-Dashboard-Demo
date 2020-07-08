bfb53ca9f87ebddf32e63ce51afcc1b9777fe1d9
jason <jasoncfleebcit@gmail.com>
2020-07-08 01:16:43 -0700

    added some jss for create sensor page

--- origen/static/js/sensorcreate.js
+++ origen/static/js/sensorcreate.js
@@ -1,23 +1,31 @@
-function convert(degree) {
-    var x;
-    if (degree == "C") {
-      x = document.getElementById("c").value * 9 / 5 + 32;
-      document.getElementById("f").value = Math.round(x);
-    } else {
-      x = (document.getElementById("f").value -32) * 5 / 9;
-      document.getElementById("c").value = Math.round(x);
-    }
-}   
+$(document).ready(function () {
 
-function convert2(degree) {
-    var y;
-    if (degree == "A") {
-      y = document.getElementById("a").value * 9 / 5 + 32;
-      document.getElementById("b").value = Math.round(y);
-    } else {
-      y = (document.getElementById("b").value -32) * 5 / 9;
-      document.getElementById("a").value = Math.round(y);
-    }
-}   
+// function convert(degree) {
+//     var x;
+//     if (degree == "C") {
+//       x = document.getElementById("c").value * 9 / 5 + 32;
+//       document.getElementById("f").value = Math.round(x);
+//     } else {
+//       x = (document.getElementById("f").value -32) * 5 / 9;
+//       document.getElementById("c").value = Math.round(x);
+//     }
+// }   
 
+// function convert2(degree) {
+//     var y;
+//     if (degree == "A") {
+//       y = document.getElementById("a").value * 9 / 5 + 32;
+//       document.getElementById("b").value = Math.round(y);
+//     } else {
+//       y = (document.getElementById("b").value -32) * 5 / 9;
+//       document.getElementById("a").value = Math.round(y);
+//     }
+// } 
+
+// function myFunction() {
+//     var x = document.getElementById("mySelect").value;
+//     document.getElementById("demo").innerHTML = "You selected: " + x;
+//   }
+
+});
 
