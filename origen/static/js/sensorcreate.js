55646fd8c3ffe02d62647d14ae4ca603a0016075
jason <jasoncfleebcit@gmail.com>
2020-07-07 23:34:58 -0700

    implemented sensor create page but will replace with revised version of sensor create page by saem

--- origen/static/js/sensorcreate.js
+++ origen/static/js/sensorcreate.js
@@ -1,4 +1,23 @@
-function temperatureConverter(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
-  }
+function convert(degree) {
+    var x;
+    if (degree == "C") {
+      x = document.getElementById("c").value * 9 / 5 + 32;
+      document.getElementById("f").value = Math.round(x);
+    } else {
+      x = (document.getElementById("f").value -32) * 5 / 9;
+      document.getElementById("c").value = Math.round(x);
+    }
+}   
+
+function convert2(degree) {
+    var y;
+    if (degree == "A") {
+      y = document.getElementById("a").value * 9 / 5 + 32;
+      document.getElementById("b").value = Math.round(y);
+    } else {
+      y = (document.getElementById("b").value -32) * 5 / 9;
+      document.getElementById("a").value = Math.round(y);
+    }
+}   
+
+
