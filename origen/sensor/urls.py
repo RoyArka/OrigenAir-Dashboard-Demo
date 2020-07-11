352ac3f469bd51e6e5f138a98d8b8f760303c029
RoyArka <blueroy9@live.com>
2020-07-10 17:50:05 -0700

    added delete functionality to sensor and added delete url

--- origen/sensor/urls.py
+++ origen/sensor/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include 
 from . import views
-
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -11,4 +10,5 @@ urlpatterns = [
     path('for/<slug>/create/', views.CreateSensor.as_view(), name='create'),
     path('for/<slug>/<pk>', views.SensorDetail.as_view(), name='single'),
     path('for/<slug>/<pk>/update/', views.UpdateSensor.as_view(), name='update'),
+    path('for/<slug>/<pk>/delete/', views.DeleteSensor.as_view(), name='delete'),
 ]
