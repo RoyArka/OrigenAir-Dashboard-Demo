6ba200cdf3fa9ea4958d0f57bec99662d311d652
RoyArka <blueroy9@live.com>
2020-07-24 21:34:31 -0700

    organization name is now not updateable

--- origen/organization/forms.py
+++ origen/organization/forms.py
@@ -63,9 +63,6 @@ class OrganizationUpdateForm(forms.ModelForm):
         fields = ('name', 'email', 'phone', 'website', 'street_name', 'street_number', 'city', 'country', 'description', 'color')
         
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control',
-                                        'placeholder': 'Organization Name',
-                                        'name': 'Org-edit'}),
             'email': forms.TextInput(attrs={'class': 'form-control',
                                         'placeholder': 'Contact Email',
                                         'name': 'Org-email-edit',
