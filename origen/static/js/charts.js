0bfc7c783669a2962e3bb79cd5efd92f86417c93
jason <jasoncfleebcit@gmail.com>
2020-07-26 14:11:08 -0700

    revised amchart to include vibrant color and 5 ranges

--- origen/static/js/charts.js
+++ origen/static/js/charts.js
@@ -452,24 +452,38 @@ am4core.ready(function () {
 
   var range0 = axis.axisRanges.create();
   range0.value = 0;
-  range0.endValue = 5;
+  range0.endValue = 20;
   range0.axisFill.fillOpacity = 1;
-  range0.axisFill.fill = colorSet.getIndex(9);
+  range0.axisFill.fill = am4core.color("#03A9F4");
   range0.axisFill.zIndex = -1;
 
   var range1 = axis.axisRanges.create();
-  range1.value = 5;
-  range1.endValue = 95;
+  range1.value = 20;
+  range1.endValue = 40;
   range1.axisFill.fillOpacity = 1;
-  range1.axisFill.fill = colorSet.getIndex(16);
+  range1.axisFill.fill = am4core.color("#00C853");
   range1.axisFill.zIndex = -1;
 
   var range2 = axis.axisRanges.create();
-  range2.value = 95;
-  range2.endValue = 100;
+  range2.value = 40;
+  range2.endValue = 60;
   range2.axisFill.fillOpacity = 1;
-  range2.axisFill.fill = colorSet.getIndex(9);
+  range2.axisFill.fill = am4core.color("#FFEE58");
   range2.axisFill.zIndex = -1;
+
+  var range3 = axis.axisRanges.create();
+  range3.value = 60;
+  range3.endValue = 80;
+  range3.axisFill.fillOpacity = 1;
+  range3.axisFill.fill = am4core.color("#FF5722");
+  range3.axisFill.zIndex = -1;
+
+  var range4 = axis.axisRanges.create();
+  range4.value = 80;
+  range4.endValue = 100;
+  range4.axisFill.fillOpacity = 1;
+  range4.axisFill.fill = am4core.color("#E53935");
+  range4.axisFill.zIndex = -1;
 
   var hand = chart.hands.push(new am4charts.ClockHand());
 
