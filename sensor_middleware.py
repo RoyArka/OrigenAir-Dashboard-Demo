c265d700f7b7e64379edc0051adde406928e750e
jason <jasoncfleebcit@gmail.com>
2020-07-27 00:10:38 -0700

    fixed merge comflict

--- sensor_middleware.py
+++ sensor_middleware.py
@@ -8,11 +8,7 @@ def main(pk):
         sensor_input = random.uniform(-5, 110)
         sensor_input = round(sensor_input, 2)
 
-<<<<<<< HEAD
         url = "http://127.0.0.1:8000/sensor/api/for/final-fight/" + str(pk)
-=======
-        url = "http://127.0.0.1:8000/sensor/api/for/bc-transit/" + str(pk)
->>>>>>> development
         headers = {'Content-Type': "application/json", 'Accept': "application/json"}
         data = {}
         data['value'] = sensor_input
@@ -22,8 +18,3 @@ def main(pk):
         print(res.status_code)
         time.sleep(1)
 main(14)
-
-<<<<<<< HEAD
-=======
-main(40)
->>>>>>> development
